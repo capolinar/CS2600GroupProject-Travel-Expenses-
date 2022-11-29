09b4b5bc3618f7cc594188b45e2ced8e727c2599
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-11-29 15:16:51 -0800

    Created Outline of functions

---
+++ prompt.c
@@ -0,0 +1,62 @@
+// int totalDays()
+// No parameter
+// Prompts user for total number of days spent on the trip
+// Returns total days
+
+
+
+// void departTime(int *departTime, int *arriveTime)
+// No return
+// Prompts user for departure time and arrival time
+// Sets pointers to these values
+
+
+
+// double airFee()
+// No parameter
+// Prompts user for the cost of airfare
+// Returns airfare price
+
+
+
+// int carsRented()
+// No parameter
+// Prompts user for total number of cars rented
+// Returns number of cars rented
+
+
+// int milesDriven()
+// No parameter
+// Prompts user for total miles driven (if using private vehicle)
+// Returns miles driven
+
+
+// double parkingFee()
+// No parameter
+// Prompts user for parking fees
+// Returns parking fees
+
+
+// double taxiFee()
+// No parameter
+// Prompts user for taxi fees
+// Returns taxi fees
+
+
+// double conferenceFee()
+// No parameter
+// Prompts user for conference fees
+// Returns conference fees
+
+
+// double hotelFee()
+// No parameter
+// Prompts user for hotel fee
+// Returns conference fees
+
+
+// int mealFee()
+// No parameter
+// Prompts user for amount of all meals
+// Returns amount for each meals eaten
+// Allowed meals
