9f4e884879344fa5163e4646976b0978b04bf4f0
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-11-30 14:08:02 -0800

    Fixed Major Issues
    - removed getTIme()
    + included string.h

--- prompt.c
+++ prompt.c
@@ -1,163 +1,117 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
 int totalDays()
 {
-    int totalDays = 0;
-    printf("Enter the number of days spent on the trip: ");
-    scanf("%d", &totalDays);
+	int totalDays = 0;
+	printf("Enter the number of days spent on the trip: ");
+	scanf("%d", &totalDays);
 
-    return totalDays;
+	return totalDays;
 }
 
-int getTime(char *buff) {
-    int hour;
-    int minute;
-    char * token;
-
-    if (token != NULL) {
-        token = strtok(buff, ":");
-        hour = atoi(token);
-        token = strtok(NULL, ":");
-        minute = atoi(token);
-    }
-    return ((hour + 11) % 12 + 1) * 100 + minute;
-}
 
 void departTime(int *departTime, int *arriveTime)
 {
-    char buff[5];
+	char buff[5];
+	int hour;
+	int minute;
 
-    printf("Enter depart time: ");
-    scanf("%s", &buff);
-    
-    // converting 24 hour to 12 hour
-    int dTime = getTime(buff);
-    *departTime = dTime;
+	printf("Enter depart time (24h): ");
+	scanf("%s", &buff);
 
-    printf("Enter arrive time: ");
-    scanf("%s", &buff);
-    // converting 24 hour to 12 hour
-    int aTime = getTime(buff);
-    *arriveTime = aTime;
+	hour = atoi(strtok(buff, ":"));
+	minute = atoi(strtok(NULL, ":"));
+	// converting 24 hour to 12 hour
+	int dTime = hour * 100 + minute;
+	*departTime = dTime;
+
+	printf("Enter arrive time (24h): ");
+	scanf("%s", &buff);
+
+	hour = atoi(strtok(buff, ":"));
+	minute = atoi(strtok(NULL, ":"));
+	// converting 24 hour to 12 hour
+	int aTime = hour * 100 + minute;
+	*arriveTime = aTime;
 }
-
-
-
-// double airFee()
-// No parameter
-// Prompts user for the cost of airfare
-// Returns airfare price
 
 double airFee()
 {
-    double airFee;
-    printf("Enter air fee: ");
-    scanf("%f", &airFee);
+	double airFee;
+	printf("Enter air fee: ");
+	scanf("%f", &airFee);
 
-    return airFee;
+	return airFee;
 }
-
-// int carsRented()
-// No parameter
-// Prompts user for total number of cars rented
-// Returns number of cars rented
 
 int carsRented()
 {
-    int carsRent;
-    printf("Enter total number of cars rented: ");
-    scanf("%d", &carsRent);
+	int carsRent;
+	printf("Enter total number of cars rented: ");
+	scanf("%d", &carsRent);
 
-    return carsRent;
+	return carsRent;
 }
-
-// int milesDriven()
-// No parameter
-// Prompts user for total miles driven (if using private vehicle)
-// Returns miles driven
 
 int milesDriven()
 {
-    int miles = 0;
-    int input;
-    printf("Was a private vehicle used?\n(1) for Yes\n(2) for No");
-    scanf("%d", &input);
+	int miles = 0;
+	int input;
+	printf("Was a private vehicle used?\n(1) for Yes\n(2) for No");
+	scanf("%d", &input);
 
-    if (input == 1) {
-        printf("Enter Total Miles Driven: ");
-        scanf("%d", &miles);
-    }
+	if (input == 1)
+	{
+		printf("Enter Total Miles Driven: ");
+		scanf("%d", &miles);
+	}
 
-    return miles;
+	return miles;
 }
-
-// double parkingFee()
-// No parameter
-// Prompts user for parking fees
-// Returns parking fees
 
 double parkingFee()
 {
-    double parkingFee;
-    printf("Enter Parking Fee: ");
-    scanf("%f", &parkingFee);
+	double parkingFee;
+	printf("Enter Parking Fee: ");
+	scanf("%f", &parkingFee);
 
-    return parkingFee;
+	return parkingFee;
 }
-// double taxiFee()
-// No parameter
-// Prompts user for taxi fees
-// Returns taxi fees
 
 double taxiFee()
 {
-    double taxiFee;
-    printf("Enter Taxi Fee: ");
-    scanf("%f", &taxiFee);
+	double taxiFee;
+	printf("Enter Taxi Fee: ");
+	scanf("%f", &taxiFee);
 
-    return taxiFee;
+	return taxiFee;
 }
-
-// double conferenceFee()
-// No parameter
-// Prompts user for conference fees
-// Returns conference fees
 
 double conferenceFee()
 {
-    double conferenceFee;
-    printf("Enter Conference Fee: ");
-    scanf("%f", &conferenceFee);
+	double conferenceFee;
+	printf("Enter Conference Fee: ");
+	scanf("%f", &conferenceFee);
 
-    return conferenceFee;
+	return conferenceFee;
 }
-
-// double hotelFee()
-// No parameter
-// Prompts user for hotel fee
-// Returns conference fees
 
 double hotelFee()
 {
-    double hotelFee;
-    printf("Enter Hotel Fee: ");
-    scanf("%f", &hotelFee);
+	double hotelFee;
+	printf("Enter Hotel Fee: ");
+	scanf("%f", &hotelFee);
 
-    return hotelFee;
+	return hotelFee;
 }
-// int mealFee()
-// No parameter
-// Prompts user for amount of all meals
-// Returns amount for each meals eaten
-// Allowed meals
 
 int mealFee()
 {
-    int mealFee;
-    printf("Enter Meal Fee: ");
-    scanf("%d", &mealFee);
+	int mealFee;
+	printf("Enter Meal Fee: ");
+	scanf("%d", &mealFee);
 
-    return mealFee;
+	return mealFee;
 }
