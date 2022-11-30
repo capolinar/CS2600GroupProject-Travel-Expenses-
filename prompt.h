c066ec941326f7c7d39acd67cf4dc5a024875118
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-11-30 14:22:19 -0800

    added file
    + header file

---
+++ prompt.h
@@ -0,0 +1,24 @@
+#ifndef PROMPT_H
+#define PROMPT_H
+
+int totalDays();
+
+void departTime(int *departTime, int *arriveTime);
+
+double airFee();
+
+int carsRented();
+
+int milesDriven();
+
+double parkingFee();
+
+double taxiFee();
+
+double conferenceFee();
+
+double hotelFee();
+
+int mealFee();
+
+#endif
