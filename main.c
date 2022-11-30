341288d8448cffe4c537c0eca29a8609d2f6e168
Michael <112598328+michaelhchon@users.noreply.github.com>
2022-11-30 11:54:44 -0800

    Update main.c
    3rd commit for main.c
    + created 4 new functions to check integrity of numbers inputted by user
    + created loops for each so that if it fails integrity check, it asks user again for value
    + added exit() to end of code
    - changed func name from "carRents" to "rentFee"
    - completely removed bool privateCar from code (and all parameters that contained it)

--- main.c
+++ main.c
@@ -2,20 +2,23 @@
 #include "functionstitlehere.h"
 #define MPH .27
 
+int moneyCheck(double money);
+int mileCheck(int numMiles);
+int dayCheck(int numDays);
+int timeCheck(int time);
+
 int main()
 {
     int days, dTime, aTime, rentalFee, miles, meals; //changed "rents" to "rentalFee"
     double airfare, parking, taxi, conference, hotel;
     double drivingCost;
     double total, budget, excess;
-    bool privateCar; //added boolean for private vehicle
     
-
     //setter function calls
     days = totalDays();
     departTime(&dTime, &aTime);
     airfare = airFee();
-    rentalFee = carsRents();
+    rentalFee = rentFee(); //changed func name from "carRents" to "rentFee"
     miles = milesDriven(&privateCar); //added parameter to switch bool and return amount of miles
     parking = parkingFee();
     taxi = taxiFee();
@@ -25,15 +28,16 @@ int main()
 
     //calculate costs
     drivingCost = miles * MPH;
-    total = calcTotal(days, dTime, aTime, airfare, rentalFee, miles, privateCar, parking, taxi, conference, hotel, meals); //rents to be changed to rental fees
-    budget = calcTotalAllowed(days, dTime, aTime, airfare, rentalFee, miles, privateCar, parking, taxi, conference, hotel, meals); //boolean for private vehicle use needed
-    excess = calcExcess(days, dTime, aTime, airfare, rentalFee, miles, privateCar, parking, taxi, conference, hotel, meals);
+    total = calcTotal(days, dTime, aTime, airfare, rentalFee, miles, parking, taxi, conference, hotel, meals); //rents to be changed to rental fees
+    budget = calcTotalAllowed(days, dTime, aTime, airfare, rentalFee, miles, parking, taxi, conference, hotel, meals); //boolean for private vehicle use needed
+    excess = calcExcess(days, dTime, aTime, airfare, rentalFee, miles, parking, taxi, conference, hotel, meals);
     //changed "rents" vars to updated "rentalFee" + added boolean for private vehicles
 
     //display totals
     displayTotal(total);
     displayTotalALlowed(budget);
     displayExcess(excess);
-
+    
+    exit(EXIT_SUCCESS);
     return 0;
 }
