6041ed7af4cad61e2c856b9440948ea6a0d95df0
Michael <112598328+michaelhchon@users.noreply.github.com>
2022-11-30 13:45:37 -0800

    Update main.c
    commit 4 to main
    - removed function prototypes
    + added loops to repeat functions if invalid input
    - removed excess + calcExcess
    + added display excess paramters
    + added new int var "nights"
    + added new function in calc section "nightsOnTrip"
    + added "nights" var as param to calc functions right after "days"

--- main.c
+++ main.c
@@ -2,41 +2,118 @@
 #include "functionstitlehere.h"
 #define MPH .27
 
-int moneyCheck(double money);
-int mileCheck(int numMiles);
-int dayCheck(int numDays);
-int timeCheck(int time);
-
 int main()
 {
-    int days, dTime, aTime, rentalFee, miles, meals; //changed "rents" to "rentalFee"
+    int days, nights, dTime, aTime, rentalFee, miles, meals; //changed "rents" to "rentalFee" // added "nights"
     double airfare, parking, taxi, conference, hotel;
     double drivingCost;
-    double total, budget, excess;
+    double total, budget;
+    int check; //added a check for integrity of numbers
     
     //setter function calls
-    days = totalDays();
-    departTime(&dTime, &aTime);
-    airfare = airFee();
-    rentalFee = rentFee(); //changed func name from "carRents" to "rentFee"
-    miles = milesDriven(&privateCar); //added parameter to switch bool and return amount of miles
-    parking = parkingFee();
-    taxi = taxiFee();
-    conference = conferenceFee();
-    hotel = hotelFee();
-    meals = mealFee(); //meals need to be combined with calculating the amount of meals allowed.
+    do {
+        check = 0;
+        days = totalDays();
+        if(days < 0)
+        {
+            check = 1;
+            printf("Enter value greater than 0\n");
+        }
+    } while(check == 1);
+    do {
+        check = 0;
+        departTime(&dTime, &aTime);
+        if((dTime == 0 || dtime > 24) || (aTime == 0 || aTime > 24))
+        {
+            check = 1;
+            printf("Enter valid time for departure and arrival\n");
+        }
+    } while(check == 1);
+    do {
+        check = 0;
+        airfare = airFee();
+        if(airfare < 0)
+        {
+            check = 1;
+            printf("Enter valid monetary value\n");
+        }
+    } while(check == 1);
+    do {
+        check = 0;
+        rentalFee = rentFee(); //changed func name from "carRents" to "rentFee"
+        if(rentalFee < 0)
+        {
+            check = 1;
+            printf("Enter valid monetary value\n");
+        }
+    } while(check == 1);
+    do {
+        check = 0;
+        miles = milesDriven(); //removed paramters for privateCar
+        if(miles < 0)
+        {
+            check = 1;
+            printf("Enter nonnegative mile value\n");
+        }
+    } while(check == 1);
+    do {
+        check = 0;
+        parking = parkingFee();
+        if(parking < 0)
+        {
+            check = 1;
+            printf("Enter valid monetary value\n");
+        }
+    } while(check == 1);
+    do {
+        check = 0;
+        taxi = taxiFee();
+        if(taxi < 0)
+        {
+            check = 1;
+            printf("Enter valid monetary value\n");
+        }
+    } while(check == 1);
+    do {
+        check = 0;
+        conference = conferenceFee();
+        if(conference < 0)
+        {
+            check = 1;
+            printf("Enter valid monetary value\n");
+        }
+    } while(check == 1);
+    do {
+        check = 0;
+        hotel = hotelFee();
+        if(hotel < 0)
+        {
+            check = 1;
+            printf("Enter valid monetary value\n");
+        }
+    } while(check == 1);
+    do {
+        check = 0;
+        meals = mealFee(); //meals need to be combined with calculating the amount of meals allowed.
+        if(conference < 0)
+        {
+            check = 1;
+            printf("Enter valid monetary value\n");
+        }
+    } while(check == 1);
 
     //calculate costs
     drivingCost = miles * MPH;
-    total = calcTotal(days, dTime, aTime, airfare, rentalFee, miles, parking, taxi, conference, hotel, meals); //rents to be changed to rental fees
-    budget = calcTotalAllowed(days, dTime, aTime, airfare, rentalFee, miles, parking, taxi, conference, hotel, meals); //boolean for private vehicle use needed
-    excess = calcExcess(days, dTime, aTime, airfare, rentalFee, miles, parking, taxi, conference, hotel, meals);
+    nights = nightsOnTrip(days); //added nightsOnTrip function
+    total = calcTotal(days, nights, dTime, aTime, airfare, rentalFee, miles, parking, taxi, conference, hotel, meals); //rents to be changed to rental fees
+    budget = calcTotalAllowed(days, nights, dTime, aTime, airfare, rentalFee, miles, parking, taxi, conference, hotel, meals); //boolean for private vehicle use needed
     //changed "rents" vars to updated "rentalFee" + added boolean for private vehicles
+    //removed excess = calcExcess
 
     //display totals
     displayTotal(total);
     displayTotalALlowed(budget);
-    displayExcess(excess);
+    displayExcess(total, budget);
     
     exit(EXIT_SUCCESS);
     return 0;
