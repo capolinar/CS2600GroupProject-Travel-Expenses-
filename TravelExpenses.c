24289d8244db03614fcb7e88378219205698e5cf
johnhuang0027 <112910208+johnhuang0027@users.noreply.github.com>
2022-12-01 00:14:41 -0800

    Create TravelExpenses.c

---
+++ TravelExpenses.c
@@ -0,0 +1,52 @@
+//headers
+
+
+//functions here
+//total expenses calculation
+//total allowable expenses calculation(departure/arrival times)
+//excess that must be reimbursed, if any calculation
+//amount saved by employee if under total allowed calculation
+
+
+
+
+
+
+
+
+
+
+//main
+
+//for any user input, input validation required(no negatives for money or miles)
+//at least 1 for number of days traveled
+//only valid times for departure/arrival(no negative times)
+
+//ask for number of days on trip
+
+// ask for departure and arrival times
+
+//ask for amount of round-trip airfare
+
+//ask for amount of any car rentals
+
+//ask for miles driven, if private vehicle used, calculate expense as .27 cents a mile
+
+//ask for parking fees
+
+//ask for taxi fees
+
+//ask conference or seminar fees
+
+//ask for hotel expneses
+
+//ask for amount of each meal eaten(only allowable)
+
+//first day, breakfast is allowed if departure is before 7
+//lunch if departure is before 12 noon
+//dinner if before 6 pm
+
+//last day, breakfast is allowed if arrival is after 8am
+//lunch if after 1pm
+//dinner if after 7pm
+
